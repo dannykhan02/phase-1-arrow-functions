14ec8fb25e42a0fde5d459f1b522bacff59e3324
dan <dannykhan614@gmail.com>
2024-03-23 12:21:35 +0300

    create my arrow function

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let a = 2000
+let b = 100
+const divide = function(a, b) {
+  return a / b;
+}
+let x =2 
+const square = (x) => {
+  return x * x;
+}
+let c = 3
+let d = 4
+const add = (a, b) => {
+  return a + b;
+}
